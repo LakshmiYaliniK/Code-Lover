bdaa151c15130a91a4754c2129c00d59a713f562
LakshmiYaliniK <99485740+LakshmiYaliniK@users.noreply.github.com>
2022-02-12 21:41:14 +0530

    Program to find grade
    Enter the mark and find your grade.

--- Grade.c
+++ Grade.c
@@ -7,23 +7,23 @@ int main()
     scanf("%d",&marks);
     if(marks>=85 &&marks<=100)
     {
-        printf("You scored Grade A");
+        printf("Grade A");
     }
     else if(marks>=70 &&marks<=84)
     { 
-        printf("You scored Grade B");
+        printf("Grade B");
     }
     else if(marks>=55 &&marks<=69)
     {
-        printf("You scored Grade C");
+        printf("Grade C");
     }
     else if(marks>=40 &&marks<=54)
     {
-        printf("You scored Grade D");
+        printf("Grade D");
     }
     else
     {
-        printf("You scored Grade F");
+        printf("Grade F");
     }
     return 0;
 }
