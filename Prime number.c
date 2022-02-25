4f3f4f7a8121139a25147f2c48d3b535d13b4174
LakshmiYaliniK <99485740+LakshmiYaliniK@users.noreply.github.com>
2022-02-25 20:41:07 +0530

    Whether a number is prime or not using recursion
    Enter a number and the output gives whether it is prime or not prime.

---
+++ Prime number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int isPrime(int,int);
+int main()
+{
+    int num, prime;
+    printf("Enter the prime number:");
+    scanf("%d",&num);
+    if(num==0||num==1)
+    printf("\n %d is not prime number:",num);
+    else
+    {
+        prime=isPrime(num,num/2);
+        if(prime==1)
+        printf("\n %d is prime number",num);
+        else
+        printf("\n %d is not prime number",num);
+    }
+    return 0;
+}
+int isPrime(int num,int i)
+{
+    if(i==1)
+    {
+        return 1;
+    }
+    else
+    {
+        if(num%i==0)
+        return 0;
+        else
+        isPrime(num,i-1);
+    }
+}
