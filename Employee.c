7ea407517c35ecacd0168ef1101c6ec84cdf389b
LakshmiYaliniK <99485740+LakshmiYaliniK@users.noreply.github.com>
2022-02-27 13:25:38 +0530

    Create Employee.c

---
+++ Employee.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+	struct employee
+	{
+		int salary,id;
+		char name[100], departmentname[100];
+	}s[100],s1[100];
+	int n,i,j,index=0;
+	printf("Enter number total no. of Employee=\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("Employee[%d]-id=\n",i);
+		scanf("%d",&s[i].id);
+		
+		printf("Employee[%d]-name=\n",i);
+		scanf("%s",s[i].name);
+		printf("Employee[%d]-departmentname=\n",i);
+		scanf("%s",s[i].departmentname);
+		printf("Employee[%d]-salary=\n",i);
+		scanf("%d",&s[i].salary);
+	}
+	
+	for(i=0;i<=n;i++)
+	{
+		s1[i]=s[i];
+	
+	}
+
+	printf("\nID\tNAME\tDEPARTMENT_NAME\tSALARY\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t%s\t%d\n",s[i].id,s[i].name,s[i]. departmentname,s[i].salary);
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-1-i;j++)
+		{
+			if(s[j].salary<s[j+1].salary)
+			{
+				s[j].salary=s[j].salary ^ s[j+1].salary;
+				s[j+1].salary=s[j].salary ^ s[j+1].salary;
+				s[j].salary=s[j].salary ^ s[j+1].salary;
+			}
+		}
+	}
+	return 0;
+}
